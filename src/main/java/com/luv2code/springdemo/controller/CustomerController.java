1d5903a06512f4a9b49fc21cf1678e8923bc9bf5
cv <amancv17@gmail.com>
2020-10-17 21:53:37 +0530

    First commit
    project compleated up to Add functionality

---
+++ src/main/java/com/luv2code/springdemo/controller/CustomerController.java
@@ -0,0 +1,51 @@
+package com.luv2code.springdemo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.luv2code.springdemo.entity.Customer;
+import com.luv2code.springdemo.service.CustomerService;
+
+@Controller
+@RequestMapping("/customer")
+public class CustomerController {
+
+	@Autowired
+	CustomerService customerservice;
+
+	@GetMapping("/list")
+	public String listCustomer(Model themodel) {
+
+		List<Customer> theCustomer = customerservice.getCustomers();
+
+		themodel.addAttribute("customer", theCustomer);
+
+		return "list-customer";
+	}
+
+	@GetMapping("/showFormForAdd")
+	public String showFormForAdd(Model theModel) {
+
+		Customer thecustomer = new Customer();
+
+		theModel.addAttribute("customer", thecustomer);
+
+		return "customer-form";
+     }
+
+	@PostMapping("/saveCustomer")
+	public String saveCustomer(@ModelAttribute("customer") Customer thecustomer) {
+		
+		customerservice.saveCustomer(thecustomer);
+		return "redirect:/customer/list";
+		
+	}
+	
+}
